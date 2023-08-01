920f1d7099854d20c09210d4ffb43705d635632b
Krishna8957 <krishnapal7576@gmail.com>
2023-07-31 20:09:24 -0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const Note = require("./modles/Note");
+
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+mongoose.connect("mongodb+srv://krishnapal1234:kk1234@cluster0.mhnx7cz.mongodb.net/backsdb").then(function()
+{
+    app.get("/",function(req, res){
+       const response = { message: "API Works!"};
+       res.json(response);
+    });
+    const noteRouter = require("./routes/Note")
+    app.use("/notes",noteRouter);
+});
+
+
+app.listen(5000,function(){
+    console.log("Server Started ar Port:5000")
+});
